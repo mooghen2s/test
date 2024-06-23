f88230553ae009ea0219a78c64c1b9fe425179cd
mooghen2s <167309023+mooghen2s@users.noreply.github.com>
2024-06-24 05:58:13 +0700

    [skip ci]

--- main.py
+++ main.py
@@ -13,17 +13,22 @@ def nonton_youtube(profile_dir):
         json_file = f'videos.json'
         with open(json_file, 'r') as file:
             videos = json.load(file)
+            user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6310.207 Safari/537.36"
             random_video = random.choice(videos)
             url = random_video['url']
             if profile_dir == 'Profile1':
                 window_pos = "0000,1000"
+                user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6310.207 Safari/537.36"
             if profile_dir == 'Profile2':
                 window_pos = "0000,1500"
+                user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6294.219 Safari/537.36"
             if profile_dir == 'Profile3':
                 window_pos = "0000,2000"
+                user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3"
             if profile_dir == 'Profile4':
-                window_pos = "0000,000"
-            print(url)            
+                window_pos = "0000,2500"
+                user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.61 Safari/537.36"
+        print(url)            
         current_directory = os.getcwd()
         start_time_yt = random.randrange(1, 400)
         end_time_yt = random.randrange(1, 200)
@@ -35,7 +40,10 @@ def nonton_youtube(profile_dir):
             f"--profile-directory={profile_dir}",
             f"--app={Url_browser}",
             f"--autoplay-policy=no-user-gesture-required",
+            '--disable-extensions',
+            f'--user-agent={user_agent}',
             #f"--window-position={window_pos}"
+            
         ]
         chrome_process = subprocess.Popen(command)
     except Exception as e:
@@ -49,7 +57,7 @@ def nonton_youtube(profile_dir):
 
     #keyboard.press_and_release('k') 
     # Tangkap layar dan simpan sebagai PNG
-    time.sleep(650)
+    time.sleep(10)
     print(profile_dir)
     with mss.mss() as sct:
         screenshot = sct.shot(output='screenshot.png')
@@ -57,5 +65,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Nonton YouTube dengan profil tertentu.')
     parser.add_argument('profile_number', type=int, help='Nomor profil yang akan digunakan')
     args = parser.parse_args()
-    profile_dir = f'Profile{args.profile_number}'
-    nonton_youtube(profile_dir)
+    profile_loop = f'Profile{args.profile_number}'
+    for nujsi in range(1, 5)
+        profile_dir = f'Profile{nujsi}'
+        nonton_youtube(profile_dir)
+    if profile_loop == 'Profile0':
+        time.sleep(650)
